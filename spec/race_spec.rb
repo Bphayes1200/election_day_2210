13178af2d3ddf068b4b309401866f149835d2511
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-01 09:47:25 -0600

    Feat: add registar candidate method and tests for class Race

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -14,5 +14,22 @@ RSpec.describe Race do
 
       expect(race.office).to eq("Texas Governor")
     end 
+
+    it 'will have candidates' do 
+      race = Race.new("Texas Governor")
+
+      expect(race.candidates).to eq([])
+    end 
+  end 
+
+  describe "#registar candidates" do 
+    it 'will register candidates into the race' do 
+      race = Race.new("Texas Governor")
+      candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = race.register_candidate!({name: "Roberto R", party: :republican})
+ 
+      expect(race.candidates[0][:name]).to eq("Diana D")
+      expect(race.candidates[1][:name]).to eq("Roberto R")
+    end 
   end 
 end 
